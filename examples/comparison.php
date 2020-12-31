03e520ab6d0471856ae173e3c0262605e23d2317
Edwin J <64994681+ed963@users.noreply.github.com>
2020-12-31 16:23:20 -0500

    Added comparison example

---
+++ examples/comparison.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+An example script to compare the usage and ouput of the smoothImageArc and GD imagearc functions. 
+*/
+
+include('./smoothImageArc.php');
+
+// create a 1000-by-1000 pixel image
+$img = imagecreatetruecolor(1000, 1000);
+
+// draw a simple arc using imagearc
+$blue = imagecolorallocate($img, 0, 0, 255);
+imagesetthickness($img, 10);
+imagearc($img, 100, 600, 500, 500, -90, 0, $blue);
+
+// draw the same arc using smoothImageArc
+$blueArray = array(0, 0, 255, 0);
+imagesetthickness($img, 1);     // line thickness must be set to 1 pixel
+smoothImageArc($img, 600, 600, 10, 250, -90, 0, $blueArray);
+
+// save image to file
+header("Content-type: image/png");
+imagepng($img, "comparison.png");
+
+// free memory
+imagedestroy($img);
+
+?>
